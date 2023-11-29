90bcaa256c79e86b8898efca99765b64a2c02aed
Anvers44 <bileldjebar@gmail.com>
2023-11-29 21:08:30 +0100

    Second Commit

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Afficher le loader
-  document.querySelector('.loader').style.display = 'flex';
 
-  // Simuler une tâche longue (à remplacer par le chargement réel de votre contenu)
-  setTimeout(function() {
-      // Masquer le loader
-      document.querySelector('.loader').style.display = 'none';
-      // Afficher le contenu
-      document.querySelector('.content').style.display = 'block';
-  }, 4500); // 2000 millisecondes (2 secondes) pour simuler un chargement
-});
 
 
 
