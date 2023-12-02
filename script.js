4ad06899494f2f6b2dba404597371046ee642d8c
Anvers44 <bileldjebar@gmail.com>
2023-12-02 18:28:21 +0100

    First final version

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-} 
+
 
 
 
@@ -35,14 +25,38 @@ window.onscroll = function() {
 
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const chiffreContainer = document.getElementById("chiffre");
-    const chiffreSpans = chiffreContainer.querySelectorAll("h4 span");
+  document.addEventListener("DOMContentLoaded", function() {
+    // Afficher le loader
+    document.querySelector('.loader').style.display = 'flex';
 
-    chiffreSpans.forEach((span, index) => {
-        setTimeout(() => {
-            span.classList.add("active");
-        }, index * 500); // Définissez la durée souhaitée entre chaque chiffre
-    });
+    // Simuler une tâche longue (à remplacer par le chargement réel de votre contenu)
+    setTimeout(function() {
+        // Masquer le loader
+        document.querySelector('.loader').style.display = 'none';
+        // Afficher le contenu
+        document.querySelector('.content').style.display = 'block';
+    }, 5000); // 2000 millisecondes (2 secondes) pour simuler un chargement
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.getElementById('navbar');
+  let isNavbarVisible = true;
+  let lastScrollPosition = 0;
+
+  window.addEventListener('scroll', function () {
+    const scrollPosition = document.documentElement.scrollTop;
+
+    if (scrollPosition > lastScrollPosition && scrollPosition > 120 && isNavbarVisible) {
+      navbar.style.top = '-9.6vh'; // Masquer la barre de navigation
+      isNavbarVisible = false;
+    } else if ((scrollPosition <= lastScrollPosition || scrollPosition <= 120) && !isNavbarVisible) {
+      navbar.style.top = '0'; // Afficher la barre de navigation
+      isNavbarVisible = true;
+    }
+
+    lastScrollPosition = scrollPosition;
+  });
 });
 
